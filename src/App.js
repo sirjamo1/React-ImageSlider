02e82d0e00e8fc149c1b09a9019f98d66566bcc4
Sirjamo1 <jam_how@hotmail.com>
2022-06-20 15:40:06 +1000

    followed along with the youtube video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Header from "./components/Header.js";
+import "./App.css"
+import Slider from "./components/slider/Slider"
 
 export default function App() {
   return (
-    <div className="card--main-container">
-      <Header />
-    </div>
+    <>
+    <Slider />
+    </>
   );
 }
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <div className="Header">
-      <h3>Header name here</h3>
-    </div>
-  );
-}
